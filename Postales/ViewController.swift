81fa99b88e51cc320cc2643b443596de2e8c4108
Edgard Maciel <Edgard@Black-Beauty.local>
2015-10-02 02:24:01 -0500

    Primera revision del proyecto

--- Postales/ViewController.swift
+++ Postales/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextFiel: UITextField!
+    
+    @IBOutlet weak var mailButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.text = enterNameTextField.text
+        messageLabel.hidden=false
+        messageLabel.textColor=UIColor.redColor()
+        
+        enterNameTextField.text=""
+        enterNameTextField.resignFirstResponder()
+        mailButton.setTitle("Mensaje Enviado!", forState: UIControlState.Normal)
+    }
 
 }
 
